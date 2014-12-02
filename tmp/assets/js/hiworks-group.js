ac1dc6e8c89bec0923498bb4cf74a07a0fb9e96c
markupbang <css@markupbang.com>
2014-12-02 16:06:09 +0900

    .

--- tmp/assets/js/hiworks-group.js
+++ tmp/assets/js/hiworks-group.js
@@ -3,14 +3,14 @@ $(function() {
 	var nice = $("html").niceScroll({cursorcolor:"#aaa",cursorwidth:"8px"});  // The document page (body)
 	$("#noticeArea").niceScroll({cursorborder:"",cursorcolor:"#aaa",cursorwidth:"8px",boxzoom:false}); // First scrollable DIV
 
-
 	$('.boxArea textarea').focus(function(){
         $(this).parent().parent('.boxArea').addClass('boxFocus');
+		$('.btnRight').removeClass('hide');
     });
 	$('.boxArea textarea').blur(function(){
         $('.boxArea').removeClass('boxFocus');
+		$('.btnRight').addClass('hide');
     });
-
 
 	//search focus in-out
 	$('.group-search input[type="text"]').focus(function(){
@@ -23,25 +23,22 @@ $(function() {
 	//header slideLayer
 	$('.groupSet').click(function(event){
 		event.stopPropagation();
-		$('.group-detail').slideToggle();
+		$('.group-detail').slideToggle('fast');
 	});		
 	$('.group-detail').click(function(event) {
          event.stopPropagation();
     });
-
 	$('.userbtn').click(function(){
 		event.stopPropagation();
-		$('.user-detail').slideToggle();
+		$('.user-detail').slideToggle('fast');
 	});
 	$('.user-detail').click(function(event) {
          event.stopPropagation();
     });
-
-	$(window).click(function(event) {
-		$('.group-detail').slideUp();
-		$('.user-detail').slideUp();
+	var clickOrTap = ( typeof document.body.ontouchend === "undefined" ) ? 'click' : 'touchend';
+		$( "body" ).on( clickOrTap, function(event) {
+		$('.group-detail').slideUp('fast');
+		$('.user-detail').slideUp('fast');
 	});
-	
-
   
 });
